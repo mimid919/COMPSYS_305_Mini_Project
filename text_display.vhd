9a68a3b9f0672823f734e878e55fcd0fcfe4193e
Soriyana Hor <shor574@UoA.auckland.ac.nz>
2026-05-11 19:09:14 +1200

    Added text display using char_rom

--- text_display.vhd
+++ text_display.vhd
@@ -11,52 +11,40 @@ ENTITY text_display IS
 END text_display;
 
 architecture behaviour of text_display is
+
+COMPONENT char_rom IS
+	PORT
+	(
+		character_address	:	IN STD_LOGIC_VECTOR (5 DOWNTO 0);
+		font_row, font_col	:	IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+		clock				: 	IN STD_LOGIC ;
+		rom_mux_output		:	OUT STD_LOGIC
+	);
+END COMPONENT char_rom;
+
+
 SIGNAL text_on : std_logic;
+SIGNAL rom_pixel : std_logic;
 
 Begin 
 
 --displaying text
-text_on <= '1' when 
-(pixel_column >= CONV_STD_LOGIC_VECTOR(200,10) 
-and
-    pixel_column <  CONV_STD_LOGIC_VECTOR(216,10)
-and
-    pixel_row    >= CONV_STD_LOGIC_VECTOR(50,10)
-and
-    pixel_row    <  CONV_STD_LOGIC_VECTOR(150,10))
-or
+text_on <= '1' when (
+    pixel_column >= CONV_STD_LOGIC_VECTOR(200,10) and
+    pixel_column <  CONV_STD_LOGIC_VECTOR(232,10) and
+    pixel_row    >= CONV_STD_LOGIC_VECTOR(100,10) and
+    pixel_row    <  CONV_STD_LOGIC_VECTOR(140,10) and
+    rom_pixel = '1'
+) else '0';
 
-(pixel_column >= CONV_STD_LOGIC_VECTOR(280,10)
-and
-    pixel_column <  CONV_STD_LOGIC_VECTOR(296,10)
-and
-    pixel_row    >= CONV_STD_LOGIC_VECTOR(50,10)
-and
-    pixel_row    <  CONV_STD_LOGIC_VECTOR(150,10))
-
-or
-
-   ( pixel_row    >= CONV_STD_LOGIC_VECTOR(95,10)
-and
-    pixel_row    <  CONV_STD_LOGIC_VECTOR(105,10)
-and
-pixel_column >= CONV_STD_LOGIC_VECTOR(216,10)
-and
-    pixel_column <  CONV_STD_LOGIC_VECTOR(296,10))
-
-or
-
-(pixel_column >= CONV_STD_LOGIC_VECTOR(300,10)
-and
-    pixel_column <  CONV_STD_LOGIC_VECTOR(316,10)
-and
-    pixel_row    >= CONV_STD_LOGIC_VECTOR(50,10)
-and
-    pixel_row    <  CONV_STD_LOGIC_VECTOR(150,10))
-
-	
-	
-	 else '0' ;
+t: char_rom
+PORT MAP (
+    character_address => "000001",
+    font_row => pixel_row(2 downto 0),
+    font_col => pixel_column(2 downto 0),
+    clock => clk,
+    rom_mux_output => rom_pixel
+);
 
 
 Red   <= text_on;
